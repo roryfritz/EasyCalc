30cc6397568a719db1b7f17fd33a2ba9b1a8870f
roryfritz <roryfritz0321@gmail.com>
2017-08-04 15:30:27 +0800

    first commit

---
+++ app/src/main/java/rory/lim/com/easycalc/Operators.java
@@ -0,0 +1,25 @@
+package rory.lim.com.easycalc;
+
+/**
+ * Created by user on 19/7/2017.
+ */
+
+public class Operators {
+    public double Addition(double firstNumber,double secondNumber ){
+
+        return firstNumber+secondNumber;
+    }
+    public double Minus(double firstNumber,double secondNumber ){
+
+        return firstNumber-secondNumber;
+    }
+    public double Multiply(double firstNumber,double secondNumber ){
+
+        return firstNumber*secondNumber;
+    }
+    public double Divide(double firstNumber,double secondNumber ){
+
+        return firstNumber/secondNumber;
+    }
+}
+
